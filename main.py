c75ee5a45be4044692ed2ae8fff3b032bb530ebf
roZhanBaghey <165216524+roZhanBaghey@users.noreply.github.com>
2024-03-28 01:25:05 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3 
+
+#start of libs
+import simple_term_menu
+import time
+import math
+import request 
+import os 
+import subprocess
+import bsf4
+import tkinter 
+import argparse
+import sqlite3 
+import flask
+import socket
+#end of libs 
+
+#begining of the code 
+class General:
+	def __init__(self):
+		pass
+
+	def run(self):
+		pass
+
+	def clalculator(self):
+		pass
+
+	def website(self):
+		pass
+
+	def time_shower(self):
+		pass
+
+	def terminal_arguments(self):
+		pass
+
+	def GUI(self):
+		pass
+
+	def tcp_server_socket(self):
+		pass
+
+	def tcp_client_socket(self):
+		pass
+	
+	def udp_server_socket(self):
+		pass
+
+	def udp_client_socket(self):
+		pass
+
+	def main_menu(self):
+		pass
+
+	def timer(self):
+		pass
+
+#end of the code
+obj = General()
+obj.run()
